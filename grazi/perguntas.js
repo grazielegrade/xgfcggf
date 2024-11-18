e6a7f3cbbcb47e461c4c3f612ea632d390625964
graziele <147524850+grazielegrade@users.noreply.github.com>
2024-11-18 11:28:23 -0300

    Add files via upload

---
+++ grazi/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades sobre futebol',
+    'Maiores campeoes?',
+'O Brasil é o país com o maior número de títulos da Copa do Mundo da FIFA, com 5 conquistas '
+)
+
+criaCartao(
+    'Copa do mundo',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Libertadores',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Champions League',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
